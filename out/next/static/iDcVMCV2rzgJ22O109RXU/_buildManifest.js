ab5f841320c2f1c88fc4e2977c58967de4111a9c
Nitin Choudhary <nitinchoudhary22112004@gmail.com>
2024-12-30 04:03:37 +0530

    code

---
+++ out/next/static/iDcVMCV2rzgJ22O109RXU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b8ae36a60bd7b5f9.css","static/chunks/pages/index-7179395dd19bde75.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
